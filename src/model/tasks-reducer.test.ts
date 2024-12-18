982beddf946c163372224e7e07720e8b7fe100c4
Aleksei Naumyshev <naumyshev@yandex.ru>
2024-12-18 10:27:31 +0300

    added case "ADD-TASK"

--- src/model/tasks-reducer.test.ts
+++ src/model/tasks-reducer.test.ts
@@ -1,4 +1,4 @@
-import { removeTaskAC, tasksReducer } from './tasks-reducer'
+import {addTaskAC, removeTaskAC, tasksReducer} from './tasks-reducer'
 import { TasksStateType } from '../App'
 
 test('correct task should be deleted from correct array', () => {
@@ -28,4 +28,28 @@ test('correct task should be deleted from correct array', () => {
             { id: '3', title: 'tea', isDone: false },
         ],
     })
+})
+
+
+test('correct task should be added to correct array', () => {
+    const startState: TasksStateType = {
+        todolistId1: [
+            { id: '1', title: 'CSS', isDone: false },
+            { id: '2', title: 'JS', isDone: true },
+            { id: '3', title: 'React', isDone: false },
+        ],
+        todolistId2: [
+            { id: '1', title: 'bread', isDone: false },
+            { id: '2', title: 'milk', isDone: true },
+            { id: '3', title: 'tea', isDone: false },
+        ],
+    }
+
+    const endState = tasksReducer(startState, addTaskAC({ title: 'juce', todolistId: 'todolistId2' }))
+
+    expect(endState['todolistId1'].length).toBe(3)
+    expect(endState['todolistId2'].length).toBe(4)
+    expect(endState['todolistId2'][0].id).toBeDefined()
+    expect(endState['todolistId2'][0].title).toBe('juce')
+    expect(endState['todolistId2'][0].isDone).toBe(false)
 })
--- src/model/tasks-reducer.ts
+++ src/model/tasks-reducer.ts
@@ -1,9 +1,21 @@
-import { TasksStateType } from '../App'
+import {TasksStateType} from '../App'
+import {v1} from "uuid";
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
     switch (action.type) {
         case 'REMOVE-TASK': {
-            return {...state, [action.payload.todolistId]: state[action.payload.todolistId].filter(t => t.id !== action.payload.taskId)}
+            return {
+                ...state,
+                [action.payload.todolistId]: state[action.payload.todolistId].filter(t => t.id !== action.payload.taskId)
+            }
+        }
+
+        case "ADD-TASK": {
+            return {
+                ...state,
+                [action.payload.todolistId]: [{id: v1(), title: action.payload.title, isDone: false},
+                    ...state[action.payload.todolistId]]
+            }
         }
 
         default:
@@ -12,11 +24,17 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksS
 }
 
 // Action creators
-export const removeTaskAC = (payload: {taskId: string, todolistId: string}) => {
-    return { type: 'REMOVE-TASK', payload} as const
+export const removeTaskAC = (payload: { taskId: string, todolistId: string }) => {
+    return {type: 'REMOVE-TASK', payload} as const
+}
+
+export const addTaskAC = (payload: { title: string, todolistId: string }) => {
+    return {type: 'ADD-TASK', payload} as const
 }
 
 // Actions types
 export type RemoveTaskActionType = ReturnType<typeof removeTaskAC>
+export type AddTaskActionType = ReturnType<typeof addTaskAC>
 
-type ActionsType = RemoveTaskActionType
+type ActionsType = RemoveTaskActionType |
+    AddTaskActionType
