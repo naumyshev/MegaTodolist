e10e772da1face5b73d9b65a5317b1f795bfc9bd
Aleksei Naumyshev <naumyshev@yandex.ru>
2024-12-26 09:29:00 +0300

    added Provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppWithRedux from "./AppWithRedux";
+import {Provider} from "react-redux";
+import {store} from "./app/store";
 
 
 const root = ReactDOM.createRoot(
@@ -9,7 +11,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <AppWithRedux />
+    <Provider store={store}>
+        <AppWithRedux />
+    </Provider>
+
   </React.StrictMode>
 );
 
